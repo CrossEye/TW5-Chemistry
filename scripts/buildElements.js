3e7698fcbfa360d5a474608b59582655d4b4c385
Scott Sauyet <scott@sauyet.com>
2023-02-13 19:16:11 -0500

    Add v4, with ElementTypes

--- scripts/buildElements.js
+++ scripts/buildElements.js
@@ -1,28 +1,44 @@
 const {writeFile, mkdir, rm,} = require ('fs/promises')
 const tap = (fn) => (x) => ((fn (x)), x)
 const map = (fn) => (xs) => xs .map (x => fn (x))
+const call = (fn, ...args) => fn (...args)
 const display = msg => tap (() => console .log (msg))
 const allPromises = (ps) => Promise .all (ps)
 
 const main = (elements) =>
   deleteOutputDir ()   // ensure there's no detritus from previous runs
     .then (createOutputDir)
-    .then (() => Promise .resolve (elements))
     .then (display ('Built directory'))
+    .then (() => Promise .resolve (elements))
     .then (addFields)
     .then (display ('Added title and tags'))
-    .then (map(addText))
+    .then (map (addText))
     .then (allPromises)
-    .then (display ('Added text from Wikipedia extracts'))
-    .then (map (writeTiddler))
+    .then (display ('Added text from Wikipedia extracts to element tiddlers'))
+    .then (map (addTimestamps))
+    .then (display ('Added created/modified timestamps'))
+    .then (tap (map (writeTiddler ('ChemicalElements'))))
     .then (allPromises)
-    .then (display (`Wrote ${elements.length} tiddlers`))
+    .then (display (`Wrote ${elements.length} element tiddlers`))
+    .then (collectTypes)
+    .then (display (`Gathered element types`))
+    .then (map (addTimestamps))
+    .then (display (`Added created/modified timestamps`))
+    .then (map (addText))
+    .then (display (`Added text from Wikipedia extracts to element type tiddlers`))
+    .then (allPromises)
+    .then (map (addLinks (elements)))
+    .then (display (`Added text links to the elements`))
+    .then (tap (map (writeTiddler ('ElementTypes'))))
+    .then (tap ((types) => console .log (`Wrote ${types.length} element type tiddlers`)))
 
 const deleteOutputDir = () => 
   rm ('./plugins/Elements/ChemicalElements', {force: true, recursive: true})
+  .then (() => rm ('./plugins/Elements/ElementTypes', {force: true, recursive: true}))
 
 const createOutputDir = () =>
   mkdir ('./plugins/Elements/ChemicalElements', {recursive: true})
+  .then (() => mkdir ('./plugins/Elements/ElementTypes', {recursive: true}))
 
 const addFields = (elements) =>
   elements .map (e => ({title: e.element, tags: '[[Chemical Element]]', ...e}))
@@ -33,17 +49,21 @@ const addText = (e) =>
     .then (o => ({
       ... e,
       ... (o .extract 
-        ? {text: '> ' + o.extract .split ('\n') .join ('\n> ') + `\n\n> ([[more information from Wikipedia|https://en.wikipedia.org/wiki/${wikipediaTitle (e .element)}]])`}
+        ? {text: '> ' + o.extract .split ('\n') .join ('\n> ') + `\n\n> ([[more information from Wikipedia|https://en.wikipedia.org/wiki/${wikipediaTitle (e .title)}]])`}
         : {}
       )
     }))
 
+const addTimestamps = (e) => call (
+  (now = new Date().toISOString().replace(/\D/g, '')) => ({...e, created: now, modified: now})
+)
+
 const wikipediaTitle = ((conversions = {
   Mercury: 'Mercury_(element)' // just the one?
-}) => (title) => conversions [title] || title) ()
+}) => (title) => conversions [title] || title .replace (/ ([A-Z])/g, (_, c) => `_${c.toLowerCase()}`)) ()
 
-const writeTiddler = (t) => 
-  writeFile (`./plugins/Elements/ChemicalElements/${t.title}.tid`, formatTiddler (t))
+const writeTiddler = (folder) => (t) => 
+  writeFile (`./plugins/Elements/${folder}/${t.title}.tid`, formatTiddler (t))
 
 const formatTiddler = ({title, text, ...rest}) =>
    [['title', title]] .concat (Object .entries (rest) 
@@ -51,5 +71,13 @@ const formatTiddler = ({title, text, ...rest}) =>
      .map (([k, v]) => `${k}: ${v}`) .join ('\n') 
      + '\n\n' + text
 
+const collectTypes = (ts) => 
+  [... new Set (ts .map (t => t ['element-type']))]
+    .map (t => ({title: t, tags: '[[Element Type]]'}))
 
+const addLinks = (elements) => ({text, ...rest}) => ({
+  ...rest,
+  text: elements .reduce ((a, {element}) => a.replaceAll (new RegExp(`\\b${element}\\b`, `gi`), (s) => `[[${s}|${element}]]`), text || '')
+})
+   
 main (require ('./../RawData/raw.json'))
