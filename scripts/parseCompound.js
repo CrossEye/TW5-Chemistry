4529608ab272c90070a7e0dc9432f449816dbbd4
Scott Sauyet <scott@sauyet.com>
2023-03-26 22:20:25 -0400

    Begin Compound work

---
+++ scripts/parseCompound.js
@@ -0,0 +1,38 @@
+const cheerio = require ('cheerio')
+
+const parse = (name) =>
+  fetch (`https://en.wikipedia.org/wiki/${name .trim () .replace (/\s/g, '_')}`)
+    .then (r => r .text())
+    .then (cheerio .load)
+    .then (getProps)
+    .then (o => ({name, ...o}))
+    .catch
+
+
+const getProps = ((props) => ($, cells = [ ... $('.infobox td')]) => {
+  return Object .fromEntries (Object .entries (props) .flatMap (([k, vs]) => {
+    const res = getProp ($, cells, vs)
+    return res ? [[k, res]] : []
+  }))
+}) ({
+  'boiling-point': ['Boiling point'],
+  'density': ['Density'],
+  'formula': ['Formula', 'Chemical formula'],
+  'melting-point': ['Melting point'],
+  'molar-mass': ['Molar mass'],
+})
+
+const getProp = ($, cells, keys) => {
+  const idx = cells .findIndex (td => keys .includes ($(td) .text() .trim()))
+  return idx > -1 ? $(cells [ idx + 1]) .text() .trim() : undefined
+}
+
+module .exports = parse
+
+
+/*
+parse ('Adenosine triphosphate')
+  .then (console .log)
+  .catch (console .warn)
+*/
+
--- scripts/parseCompounds.js
+++ scripts/parseCompounds.js
@@ -1,42 +1,11 @@
-const cheerio = require ('cheerio')
-// bad idea, but may be useful behind corporate walls
-// process .env ["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+const names = require ('./../RawData/compoundNames.json')
+const parseCompound = require ('./../scripts/parseCompound')
 
-const tap = (fn) => (x) => (fn (x), x)
-const findIndices = (fn) => ($, xs) => xs .reduce (
-    (a, x, i) => [...a, ... (fn(x) ? [i] : [])], 
-    []
-)
+const main = (names) =>
+  Promise .all (Object .entries (names) .map (([k, v]) => parseCompound (v)))
+    // .then (console .log)
+    
 
-// Trying to scrape Wikipedia's compound page
-
-const processTable = ($) => {
-  const rows = [ ... $('.infobox tr')]
-  const indices = findIndices (tr => {
-    const th = $(tr) .find ('th:first-child') 
-    return th && th .attribs .colspan == '2'
-    // td.attribs.colspan == '2'
-  }) ($, rows)
-  console .log ({indices})
-  /*
-  return rows .forEach ((elem, idx) => {
-    const value = $ (elem) .text() .trim()
-    console .log ({idx, value})
-    return {idx, value}
-  })
-  */
-  return indices .map (i => $ (rows [i]) .text() )
-}
-
-
-const parse = (name) =>
-  fetch (`https://en.wikipedia.org/wiki/${name .trim () .replace (/\s/g, '_')}`)
-    .then (r => r .text())
-//     .then (tap (console .log))
-    .then (cheerio .load)
-    .then (processTable)
-
-
-parse ('Adenosine triphosphate')
+main (names)
   .then (console .log)
   .catch (console .warn)
